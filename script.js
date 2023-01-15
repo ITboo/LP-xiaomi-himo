0fd2b585a96e104359dac377d6c67ef092837adc
ITboo <annaacdc@tut.by>
2023-01-15 20:58:18 +0300

    feat: add slider

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.about__slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000 //2s
+    });
+  });
